5044f6296e07124b17df61b06d9cd9680d2484d6
Donghao Ren <donghao.ren@gmail.com>
2017-02-04 19:18:54 -0800

    fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var os = require('os');
 var platform = os.platform();
 
 if(platform == "darwin") {
-    exports = require("./darwin_x64/allofw.node");
+    module.exports = require("./darwin_x64/allofw.node");
 }
 if(platform == "win32") {
-    exports = require("./win32_x64/allofw.node");
+    module.exports = require("./win32_x64/allofw.node");
 }
