67b8d066357c84f66a762f5f089a51df58ee6fde
Donghao Ren <donghao.ren@gmail.com>
2017-12-10 23:20:13 -0800

    update mac compability

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ var os = require('os');
 var platform = os.platform();
 
 if(platform == "darwin") {
-    module.exports = require("./darwin_x64/allofw.node");
+    if(process.version.match(/^v6.*/)) {
+        module.exports = require("./darwin_x64/allofw.v6.node");
+    }
+    if(process.version.match(/^v7.*/)) {
+        module.exports = require("./darwin_x64/allofw.v7.node");
+    }
+    if(process.version.match(/^v8.*/)) {
+        module.exports = require("./darwin_x64/allofw.v8.node");
+    }
+    if(process.version.match(/^v9.*/)) {
+        module.exports = require("./darwin_x64/allofw.v9.node");
+    }
 }
 if(platform == "win32") {
     module.exports = require("./win32_x64/allofw.node");
